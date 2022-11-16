41ee226a750d4c98175b2307a11bbce600b395c7
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-11-16 16:13:33 +0700

    make object Node class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace DoublyLinkedList
 {
-    internal class Program
+    class Node
+    {
+        public int rollNumber;
+        public String name;
+        public Node next;
+        public Node prev;
+    }
+    class Program
     {
         static void Main(string[] args)
         {
