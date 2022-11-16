2fc1b263c26ae32986b34166bcb0adae6c132bc7
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-11-16 16:17:51 +0700

    add function for input data

--- Program.cs
+++ Program.cs
@@ -20,6 +20,18 @@ namespace DoublyLinkedList
         {
             START = null;
         }
+        public void addNode()
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student : ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student : ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+        }
     }
     class Program
     {
